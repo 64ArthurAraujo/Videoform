b04c5d419787ab6ffdafa84948a938d0a6bedb9f
arthur <arthur.araujo@tutanota.com>
2022-06-29 22:14:02 -0300

    Video upload feature

--- src/main/java/org/videoform/api/controller/insert/VideoInsert.java
+++ src/main/java/org/videoform/api/controller/insert/VideoInsert.java
@@ -2,12 +2,17 @@ package org.videoform.api.controller.insert;
 
 import static org.videoform.api.configuration.Settings.REQUEST_PATH_INSERT;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping(path = REQUEST_PATH_INSERT)
 public class VideoInsert {
 
-	// Video upload
+    @RequestMapping(method = RequestMethod.POST, path = "/videos")
+    public String insertVideo(@RequestParam("video") MultipartFile video) {
+        System.out.println(video);
+
+        return video.getName();
+    }
 }
